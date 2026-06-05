6c6ab5ff7186157f0f3df30f45dd203c18f61b0e
Alex Brilevski <brilevski.alex@gmail.com>
2026-06-05 22:51:06 +0300

    Stories for testing tasks CRUD requests were added

--- src/stories/ToDoListsApi.stories.tsx
+++ src/stories/ToDoListsApi.stories.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
-import { todoListsApi } from '../api/todoListsApi';
+import { todoListsApi, type UpdateTaskRequestData } from '../api/todoListsApi';
 
 export default {
   title: 'TodoList/API',
@@ -136,6 +136,260 @@ export const UpdateTodolistTitle = () => {
           placeholder={"New Title"}
         />
         <button onClick={onUpdateToDoTitle}>Update To Do Title</button>
+      </div>
+      <div>{state}</div>
+    </>
+  );
+};
+
+export const GetTasks = () => {
+  const [todoListId, setTodoListId] = useState<string>('');
+  const [state, setState] = useState<string | null>(null);
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (todoListId.trim() !== '') {
+      todoListsApi.getTasks(todoListId).then(response => {
+        console.log('Data log - get todo list tasks', response);
+        setState(JSON.stringify(response.data));
+        inputRef.current!.value = '';
+      });
+    }
+  }, [todoListId]);
+
+  const onGetTasks = () => {
+    const value = inputRef.current?.value;
+
+    if (value) {
+      setTodoListId(value);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          ref={inputRef}
+          placeholder={"Todo List ID"}
+        />
+        <button onClick={onGetTasks}>Get Tasks</button>
+      </div>
+      <div>{state}</div>
+    </>
+  );
+};
+
+export const CreateNewTask = () => {
+  const [todoListId, setTodoListId] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [state, setState] = useState<string | null>(null);
+  const todoIdRef = useRef<HTMLInputElement | null>(null);
+  const newTitleRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (todoListId.trim() !== '' && title.trim() !== '') {
+      todoListsApi.createTask(todoListId, title).then(response => {
+        console.log('Data log - create task', response);
+        setState(JSON.stringify(response.data));
+        todoIdRef.current!.value = '';
+        newTitleRef.current!.value = '';
+      });
+    }
+  }, [todoListId, title]);
+
+  const onCreateTask = () => {
+    const todoIdValue = todoIdRef.current?.value;
+    const titleValue = newTitleRef.current?.value;
+
+    if (todoIdValue) {
+      setTodoListId(todoIdValue);
+    }
+
+    if (titleValue) {
+      setTitle(titleValue);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          ref={todoIdRef}
+          placeholder={"Todo List ID"}
+        />
+        <input
+          ref={newTitleRef}
+          placeholder={"New Task Title"}
+        />
+        <button onClick={onCreateTask}>Create Task</button>
+      </div>
+      <div>{state}</div>
+    </>
+  );
+};
+
+export const DeleteTask = () => {
+  const [todoListId, setTodoListId] = useState<string>('');
+  const [taskId, setTaskId] = useState<string>('');
+  const [state, setState] = useState<string | null>(null);
+  const todoIdRef = useRef<HTMLInputElement | null>(null);
+  const taskIdRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (todoListId.trim() !== '' && taskId.trim() !== '') {
+      todoListsApi.deleteTask(todoListId, taskId).then(response => {
+        console.log('Data log - delete task', response);
+        setState(JSON.stringify(response.data));
+        todoIdRef.current!.value = '';
+        taskIdRef.current!.value = '';
+      });
+    }
+  }, [todoListId, taskId]);
+
+  const onDeleteTask = () => {
+    const todoIdValue = todoIdRef.current?.value;
+    const taskIdValue = taskIdRef.current?.value;
+
+    if (todoIdValue) {
+      setTodoListId(todoIdValue);
+    }
+
+    if (taskIdValue) {
+      setTaskId(taskIdValue);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          ref={todoIdRef}
+          placeholder={"Todo List ID"}
+        />
+        <input
+          ref={taskIdRef}
+          placeholder={"Task ID"}
+        />
+        <button onClick={onDeleteTask}>Delete Task</button>
+      </div>
+      <div>{state}</div>
+    </>
+  );
+};
+
+export const UpdateTask = () => {
+  const initTaskState: UpdateTaskRequestData = {
+    title: '',
+    description: '',
+    status: 0,
+    priority: 0,
+    startDate: '',
+    deadline: '',
+  };
+  const [todoListId, setTodoListId] = useState<string>('');
+  const [taskId, setTaskId] = useState<string>('');
+  const [task, setTask] = useState<UpdateTaskRequestData>(initTaskState);
+  const [state, setState] = useState<string | null>(null);
+  const todoIdRef = useRef<HTMLInputElement | null>(null);
+  const taskIdRef = useRef<HTMLInputElement | null>(null);
+  const newTitleRef = useRef<HTMLInputElement | null>(null);
+  const newDescriptionRef = useRef<HTMLInputElement | null>(null);
+  const newStatusRef = useRef<HTMLInputElement | null>(null);
+  const newPriorityRef = useRef<HTMLInputElement | null>(null);
+  const newStartDateRef = useRef<HTMLInputElement | null>(null);
+  const newDeadlineRef = useRef<HTMLInputElement | null>(null);
+
+  useEffect(() => {
+    if (todoListId.trim() !== '' && taskId.trim() !== '') {
+      todoListsApi.updateTask(todoListId, taskId, task).then(response => {
+        console.log('Data log - create task', response);
+        setState(JSON.stringify(response.data));
+        todoIdRef.current!.value = '';
+        taskIdRef.current!.value = '';
+        newTitleRef.current!.value = '';
+        newDescriptionRef.current!.value = '';
+        newStatusRef.current!.value = '';
+        newStartDateRef.current!.value = '';
+        newDeadlineRef.current!.value = '';
+      });
+    }
+  }, [todoListId, taskId, task]);
+
+  const onUpdateTask = () => {
+    const todoIdValue = todoIdRef.current?.value;
+    const taskIdValue = taskIdRef.current?.value;
+    const titleValue = newTitleRef.current?.value;
+    const descriptionValue = newDescriptionRef.current?.value;
+    const statusValue = newStatusRef.current?.value;
+    const priorityValue = newPriorityRef.current?.value;
+    const startDateValue = newStartDateRef.current?.value;
+    const deadlineValue = newDeadlineRef.current?.value;
+
+    if (todoIdValue) {
+      setTodoListId(todoIdValue);
+    }
+
+    if (taskIdValue) {
+      setTaskId(taskIdValue);
+    }
+
+    if (
+      titleValue &&
+      descriptionValue &&
+      statusValue &&
+      priorityValue &&
+      startDateValue &&
+      deadlineValue
+    ) {
+      setTask({
+        title: titleValue,
+        description: descriptionValue,
+        status: +statusValue,
+        priority: +priorityValue,
+        startDate: startDateValue,
+        deadline: deadlineValue,
+      });
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <input
+          ref={todoIdRef}
+          placeholder={"Todo List ID"}
+        />
+        <input
+          ref={taskIdRef}
+          placeholder={"Task ID"}
+        />
+        <input
+          ref={newTitleRef}
+          placeholder={"New Title"}
+        />
+        <input
+          ref={newDescriptionRef}
+          placeholder={"New Description"}
+        />
+        <input
+          ref={newStatusRef}
+          type={"number"}
+          placeholder={"New Status"}
+        />
+        <input
+          ref={newPriorityRef}
+          type={"number"}
+          placeholder={"New Priority"}
+        />
+        <input
+          ref={newStartDateRef}
+          placeholder={"New Start Date"}
+        />
+        <input
+          ref={newDeadlineRef}
+          placeholder={"New Deadline"}
+        />
+        <button onClick={onUpdateTask}>Update Task</button>
       </div>
       <div>{state}</div>
     </>
